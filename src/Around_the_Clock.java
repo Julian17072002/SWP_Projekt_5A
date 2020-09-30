e8888a85e6104f20b645a8767e5fe729d18bc257
Julian17072002 <55779857+Julian17072002@users.noreply.github.com>
2020-09-30 08:44:24 +0200

    Pflichteheft

---
+++ src/Around_the_Clock.java
@@ -0,0 +1,14 @@
+
+public class Around_the_Clock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void output() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Doppel_Training.java
@@ -0,0 +1,14 @@
+
+public class Doppel_Training {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void output() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
